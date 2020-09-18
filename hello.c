3a1a5cf39e9a231662464e528df3872b8043cf10
LYF <62931549+GDOU-LYF@users.noreply.github.com>
2020-09-18 08:30:30 +0000

    C/C++

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    printf("hello wordspaces!");
+    printf("hello wordspaces!\n");
 
     return 0;
 }
---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"hello github codespace!"<<endl;
+    return 0;
+}
